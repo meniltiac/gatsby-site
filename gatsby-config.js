fb849caefa74613ba72082136529f0cac920e659
Caitlin McKenna <caitlin.elizamckenna@gmail.com>
2018-09-17 22:29:16 -0600

    Add footer, text-align center

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'CE McKenna',
+    title: 'C.E. McKenna',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Footer from '../components/footer'
 
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
